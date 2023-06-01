92343dd655a3b094ed94a3fe835b2dffbeff304e
Jupiter-J <namju2912@gmail.com>
2023-06-01 15:56:49 +0900

    [lv2]이진변환_반복

--- 프로그래머스/문자열/test.py
+++ 프로그래머스/문자열/test.py
@@ -1,17 +1,20 @@
 
-def solution(s):
-    answer = len(s)
-    for x in range(1, len(s) // 2 + 1):
-        char_len = 0
-        char = ''
-        cnt = 1
-        for i in range(0, len(s) + 1, x):
-            temp = s[i: i +x]
-            if char == temp: cnt += 1
-            elif char != temp:
-                char_len += len(temp)
-                if cnt > 1: char_len += len(str(cnt))
-                cnt = 1
-                char = temp
-        answer = min(answer, char_len)
-    return answer
+# def solution(s):
+#     answer = len(s)
+#     for x in range(1, len(s) // 2 + 1):
+#         char_len = 0
+#         char = ''
+#         cnt = 1
+#         for i in range(0, len(s) + 1, x):
+#             temp = s[i: i +x]
+#             if char == temp: cnt += 1
+#             elif char != temp:
+#                 char_len += len(temp)
+#                 if cnt > 1: char_len += len(str(cnt))
+#                 cnt = 1
+#                 char = temp
+#         answer = min(answer, char_len)
+#     return answer
+
+s=110
+print(bin(s))
