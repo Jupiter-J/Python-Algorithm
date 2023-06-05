e7ad524e8ab3ac66b9a0b9a2d2e39e0ee13c2d97
Jupiter-J <namju2912@gmail.com>
2023-06-06 03:17:00 +0900

    [lv2] 카펫

---
+++ 프로그래머스/완전탐색/카펫.py
@@ -0,0 +1,19 @@
+
+"""
+카펫
+"""
+def solution(brown, yellow):
+    s = brown * yellow
+    w,h = 0,0
+    answer = []
+    for i in range(1 , s+1):
+        #yellow 가로 높이를 구하기
+        if yellow % i ==0:
+            w = yellow//i
+            h = i
+            #각 가로 높이의 *2 +4가 brown조건과 같을때 정답
+            if 2*w + 2*h +4 == brown:
+                answer.append(w+2)
+                answer.append(h+2)
+                break
+    return answer
