30e9ef2669c4f1a39be2f93413a3895bbc9dc282
Jupiter-J <namju2912@gmail.com>
2023-06-05 15:52:44 +0900

    [lv0]배열_두배

---
+++ 프로그래머스/기초/배열_두배.py
@@ -0,0 +1,16 @@
+
+num = [1,2,3,4,5]
+result=[]
+for x in num:
+    result.append(x*2)
+print(result)
+
+
+"""
+추가 간단 코드
+"""
+
+numbers= [1,2,3,4,5]
+def solution(numbers):
+    return [x*2 for x in numbers]
+
