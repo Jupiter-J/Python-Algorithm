0dc848a4e29b1977e796412445c8b6922abb73ed
Jupiter-J <namju2912@gmail.com>
2023-06-06 12:40:53 +0900

    [lv2]가장_큰수

--- 프로그래머스/정렬/가장큰수.py
+++ 프로그래머스/정렬/가장큰수.py
@@ -4,8 +4,22 @@ join구문, lamda식
 """
 
 
-def solution(numbers):
-    numbers= list(map(str, numbers))
-    numbers.sort(key=lambda x: x*3, reverse=True)
+# def solution(numbers):
+#     numbers= list(map(str, numbers))
+#     numbers.sort(key=lambda x: x*3, reverse=True)
+#
+#     return str(int(''.join(numbers)))
 
+# numbers = [6,10,2]
+# numbers = list(map(str, numbers))
+# numbers.sort(key=lambda x: x*3, reverse=True)
+# print(str(int(''.join(numbers))))
+
+
+def solution(numbers):
+    #int list -> str list로 변환
+    numbers= list(map(str, numbers))
+    #내림차순 정렬/ 3, 30 우선순위를 바꾸기 위해 3자리 맞추기
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    #반복되는 0제거를 위해 int형 변환
     return str(int(''.join(numbers)))
