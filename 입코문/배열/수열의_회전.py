fd240d6b9fc53f266590a99833597d441d5c2c28
Jupiter-J <namju2912@gmail.com>
2023-06-12 17:14:05 +0900

    중복제거

--- 입코문/배열/수열의_회전.py
+++ 입코문/배열/수열의_회전.py
@@ -6,7 +6,6 @@ def solution(nums, k):
         answer.append(answer.popleft())
     return list(answer)
 
-
 print(solution([3, 7, 1, 5, 9, 2, 8], 3))
 print(solution([2, 12, 2, 1, 3, 3, 9], 2))
 print(solution([1, 2, 5, 4, 6, 7, 9], 6))
