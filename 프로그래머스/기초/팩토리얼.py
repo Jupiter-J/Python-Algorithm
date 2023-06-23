84aed05900cc18b374eb425853ab873c5e3990ea
Jupiter-J <namju2912@gmail.com>
2023-06-23 18:51:13 +0900

    [lv0]팩토리얼

---
+++ 프로그래머스/기초/팩토리얼.py
@@ -0,0 +1,30 @@
+
+"""
+테케 에러
+"""
+# def solution(n):
+#     answer = []
+#     save=1
+#     for x in range(1, n+1):
+#         save*=x
+#         answer.append(x)
+#         if save >n:
+#             return answer[-2]
+
+
+
+"""
+다른 코드
+"""
+
+from math import factorial
+# def solution(n):
+#     k=10
+#     while n<factorial(k):
+#         k-=1
+#     return k
+#
+# print(solution(3628800))
+
+
+print(factorial(4))
