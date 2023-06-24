f3451ddf234ffc4ef1a17965dca875b619f7077d
Jupiter-J <namju2912@gmail.com>
2023-06-24 15:39:57 +0900

    로봇의이동

---
+++ 입코문/시뮬레이션/로봇의_이동.py
@@ -0,0 +1,21 @@
+
+
+def solution(moves):
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+    x=y=0
+    d=1 #dx, dy 인덱스 번호
+    for c in moves:
+        if c == 'G':
+            x = x +dx[d]
+            y = y +dy[d]
+        elif c == 'R':
+            d = (d+1)%4
+        elif c == 'L':
+            d = (d-1)%4
+    return [x, y]
+
+print(solution('GGGRGGG'))
+print(solution('GGRGGG'))
+print(solution('GGGGGGGRGGGRGGRGGGLGGG'))
+print(solution('GGLLLGLGGG'))
