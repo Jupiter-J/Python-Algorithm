6ffd275776db5fcf56a0a69e733cdaf2c8aefc11
Jupiter-J <namju2912@gmail.com>
2023-07-03 23:12:43 +0900

    [fix]컴퓨터_개수

--- 입코문/그래프/컴퓨터_개수.py
+++ 입코문/그래프/컴퓨터_개수.py
@@ -1,9 +1,7 @@
 
-
-
-
-
-
+"""
+인접행렬
+"""
 # cnt=0
 # def DFS(v, graph, ch, n):
 #     global cnt
@@ -24,25 +22,49 @@
 #     return n-cnt #전체갯수에서 서버수 제외
 
 
-answer=0
+# answer=0
+# def DFS(v, graph, ch, n):
+#     global answer
+#     ch[v]=1
+#     answer+=1
+#     for i in range(1, n+1):
+#         if graph[v][i]==1 and ch[i]==0:
+#             DFS(i, graph, ch, n)
+# def solution(n, edges):
+#     global answer
+#     ch=[0]*(n+1)
+#     #인접행렬 그래프만들기
+#     graph=[[0]*(n+1) for _ in range(n+1)]
+#     for [x,y] in edges:
+#         graph[x][y]=1
+#         graph[y][x]=1
+#     answer = 0
+#     DFS(1, graph, ch, n)
+#     return n-answer
+
+"""
+인접리스트 - 이차원 배열 활용
+"""
+cnt=0
 def DFS(v, graph, ch, n):
-    global answer
-    ch[v]=1
-    answer+=1
-    for i in range(1, n+1):
-        if graph[v][i]==1 and ch[i]==0:
-            DFS(i, graph, ch, n)
+    global cnt
+    ch[v]=2
+    cnt +=1
+    for nv in graph[v]: #[v]=key값
+        if ch[nv]==0:
+            DFS(nv, graph, ch, n)
+
 def solution(n, edges):
-    global answer
+    global cnt
     ch=[0]*(n+1)
-    #인접행렬 그래프만들기
-    graph=[[0]*(n+1) for _ in range(n+1)]
-    for [x,y] in edges:
-        graph[x][y]=1
-        graph[y][x]=1
-    answer = 0
+    graph = [[] for _ in range(n+1)] # [[], [], [], [], [], [], [], [], [], [], [], []]
+    for [a,b] in edges:  # [[], [2, 4], [1, 3], [2], [1, 5], [4, 6], [5], [8, 10], [7, 9], [8], [7, 11], [10]]
+        graph[a].append(b)
+        graph[b].append(a)
+    cnt = 0
     DFS(1, graph, ch, n)
-    return n-answer
+    return n - cnt
+
 
 print(solution(11, [[1, 2], [1, 4], [2, 3], [4, 5], [5, 6], [7, 8], [7, 10], [8, 9], [10, 11]]))
 # print(solution(12, [[1, 2], [1, 7], [1, 8], [1, 6], [8, 11], [11, 12]]))
