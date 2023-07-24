6ec5c84f2b38fc2d7b69386c89d2546dda359fdc
Jupiter-J <namju2912@gmail.com>
2023-07-24 14:43:58 +0900

    [이코테]부품찾기

---
+++ 이코테/이진탐색/부품찾기.py
@@ -0,0 +1,22 @@
+
+
+"""
+5
+8 3 7 9 2
+3
+5 7 9
+"""
+n = int(input())
+n_arr = list(map(int, input().split()))
+m = int(input())
+m_arr = list(map(int, input().split()))
+
+answer =[]
+
+for check in m_arr:
+    if check in n_arr:
+        answer.append("YES")
+    else:
+        answer.append("NO")
+print(' '.join(answer))
+
