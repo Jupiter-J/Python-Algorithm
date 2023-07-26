41f2e385f247ef785b7938b5f1ae1549171ff538
Jupiter-J <namju2912@gmail.com>
2023-07-26 09:25:36 +0900

    [이코테]DP_example

---
+++ 이코테/DP/1로_만들기.py
@@ -0,0 +1,21 @@
+
+"""
+1로 만들기
+"""
+
+# n = int(input())
+# cnt=0
+# while n>1:
+#     if n%5==0:
+#         cnt+=1
+#         n=n//5
+#     elif n%3==0:
+#         cnt+=1
+#         n=n//3
+#     elif n%2==0:
+#         cnt+=1
+#         n=n//2
+#     else:
+#         cnt+=1
+#         n-=1
+# print(cnt)
