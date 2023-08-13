6aeb0845d8f9f88a24a06f1b8b3c08cc0cc995d8
Jupiter-J <namju2912@gmail.com>
2023-08-14 01:38:10 +0900

    공주_구하기

---
+++ 파알문/자료구조/공주_구하기.py
@@ -0,0 +1,21 @@
+
+"""
+8 3
+"""
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n, m = map(int, input().split())
+prince = [n for n in range(1, n+1)]
+prince = list(range(1, n+1))
+prince=deque(prince)
+
+while prince:
+    for i in range(n-1):
+        prince.append(prince.popleft())
+    prince.popleft()
+    if len(prince)==1:
+        break
+
+print(' '.join( map(str, list(prince))))
