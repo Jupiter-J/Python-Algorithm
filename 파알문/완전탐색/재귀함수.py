f4407b8eef688e2aae9659fe9b8cbb95829e60f8
Jupiter-J <namju2912@gmail.com>
2023-08-15 18:58:24 +0900

    이진수

---
+++ 파알문/완전탐색/재귀함수.py
@@ -0,0 +1,11 @@
+
+
+def DFS(n):
+    if n==0:
+        return
+    else:
+        DFS(n//2)
+        print(n%2, end='')
+
+n = int(input())
+DFS(n)
--- 파알문/자료구조/아나그램.py
+++ 파알문/자료구조/아나그램.py
@@ -5,22 +5,17 @@ baeeACA
 """
 a = input()
 b = input()
-word1 = dict()
-word2 = dict()
+find=dict()
+for c in a:
+    find[c]=find.get(c,0)+1
+for c in b:
+    find[c]=find.get(c,0)-1
 
 for c in a:
-    word1[c]=word1.get(c,0)+1
-for c in b:
-    word2[c]=word2.get(c,0)+1
-
-for i in word1.keys():
-    if i in word2.keys():
-        if word1[i]!=word2[i]: #value값 비교로 확인
-            print("NO")
-            break
-    else:
+    if find.get(c)>0:
         print("NO")
         break
 else:
     print("YES")
+
 
